087e1c039e9539ad26d3ec2350d551d419e590be
Isaiah <daezyworld@gmail.com>
2018-12-04 00:09:08 +0100

    fixed saving and retirieving from database

---
+++ app/Controller/topicsController.php
@@ -0,0 +1,22 @@
+<?php
+
+        class TopicsController extends AppController {
+            public $components = array('Session');
+            public function index() {
+                // Read = displaying topics created in database
+                $data = $this->Topic->find('all');
+                $this->set('topics', $data);
+                
+            }
+
+            // Creating topics in database
+            public function add() {
+                if($this -> request->is('post')) {
+                    $this->Topic->create();
+                  if($this->Topic->Save($this->request->data)){
+                      $this->Session->setFlash('The topic has been created');
+                      $this->redirect('index');
+                  };
+                }
+            }
+        }
